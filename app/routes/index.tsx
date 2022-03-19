203e6e9a14f9521abc80c316aa336b2edd46501a
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-19 20:48:17 +0530

    home sidebar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,21 @@
 import {
   Avatar,
   Box,
+  Button,
+  Flex,
   Grid,
   GridItem,
+  Heading,
   HStack,
+  Spacer,
   Text,
   VStack,
 } from "@chakra-ui/react";
 import PostCard from "~/components/Posts/PostCard";
-
+import { Link } from "remix";
 export default function Home() {
   return (
-    <Grid templateColumns="repeat(3, 1fr)">
+    <Grid templateColumns="repeat(3, 1fr)" gap={6}>
       {/* left section   */}
       <GridItem colSpan={2}>
         {/* stories section  */}
@@ -49,8 +53,66 @@ export default function Home() {
 
       {/* right section  */}
       <GridItem>
-        {/* profile section  */}
-        {/* user suggestions */}
+        <Box position={"sticky"} top={20}>
+          {/* profile section  */}
+          <Flex align={"center"} gap={4}>
+            <Avatar
+              src="https://avatars.githubusercontent.com/u/57381638?v=4"
+              size={"lg"}
+            />
+            <Box flexGrow={1}>
+              <Heading size={"xs"}>harshmangalam2022</Heading>
+              <Text fontSize={"sm"} color="gray.500">
+                Harsh Mangalam
+              </Text>
+            </Box>
+            <Button size={"xs"} colorScheme="twitter" variant={"ghost"}>
+              Switch
+            </Button>
+          </Flex>
+          {/* user suggestions */}
+          <Box my={4}>
+            <Flex align={"center"}>
+              <Heading size={"sm"} color="gray.500">
+                Suggestions for you
+              </Heading>
+              <Spacer />
+              <Heading
+                as={Link}
+                to="/explore/people"
+                color={"blue.500"}
+                size="xs"
+              >
+                See All
+              </Heading>
+            </Flex>
+
+            <VStack my={4} spacing={2} align="start">
+              {[...Array(5).keys()].map((user) => (
+                <Flex align={"center"} w="full">
+                  <HStack>
+                    <Avatar
+                      src="https://avatars.githubusercontent.com/u/57381638?v=4"
+                      size={"sm"}
+                    />
+
+                    <Box>
+                      <Heading size={"xs"}>harshmangalam2022</Heading>
+                      <Text fontSize={"sm"} color="gray.500">
+                        Follows you
+                      </Text>
+                    </Box>
+                  </HStack>
+                  <Spacer />
+
+                  <Button size={"xs"} colorScheme="twitter" variant={"ghost"}>
+                    Follow
+                  </Button>
+                </Flex>
+              ))}
+            </VStack>
+          </Box>
+        </Box>
       </GridItem>
     </Grid>
   );
